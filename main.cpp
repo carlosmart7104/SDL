60175210076bda90c001d92e0a89c992e7186d22
carlosmart7104 <carlosmart7104@gmail.com>
2016-12-05 18:08:25 -0600

    disparos funcionando

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <SDL_mixer.h>
 #include <SDL_ttf.h>
 #include <math.h>
+#include <vector>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
@@ -22,7 +23,7 @@ int main(int argc, char **argv)
 	// Obtener la imagen como una superficie
 	SDL_Surface* playerImg = IMG_Load("tileset_asteroids.png");
 	// Crear una textura en base a la superficie de la imagen (solo se puede renderizar texturas)
-	SDL_Texture* texturePlayer = SDL_CreateTextureFromSurface(render, playerImg);
+	SDL_Texture* textureTileset = SDL_CreateTextureFromSurface(render, playerImg);
 
 	// Player
 	class Player
@@ -33,18 +34,51 @@ int main(int argc, char **argv)
 		double angle = 0.0f;
 		double aceleracion = 0.1f; // pixels / seg^2
 		void update(){
-			SDL_Log("player.x: %i",this->x);
-			SDL_Log("player.y: %i",this->y);
-			SDL_Log("player.angle: %0.10lf",this->angle);
+			//SDL_Log("player.x: %i",this->x);
+			//SDL_Log("player.y: %i",this->y);
+			//SDL_Log("player.angle: %0.10lf",this->angle);
 			double incx = this->aceleracion * cos(this->angle * 0.0174533);
 			double incy = this->aceleracion * sin(this->angle * 0.0174533);
-			SDL_Log("aceleracion: %0.10lf", this->aceleracion);
-			SDL_Log("incremento en x: %0.10lf",incx);
-			SDL_Log("incremento en y: %0.10lf",incy);
+			//SDL_Log("aceleracion: %0.10lf", this->aceleracion);
+			//SDL_Log("incremento en x: %0.10lf",incx);
+			//SDL_Log("incremento en y: %0.10lf",incy);
 			this->x += incx;
 			this->y += incy;
 		}
 	} player;
+
+	class Gun
+	{
+	public:
+		double t = 0, x, y, ang, acel = 70.0f;
+		Gun(double init_x, double init_y, double init_a){
+			this->t = SDL_GetTicks();
+			this->ang =  init_a;
+			double incx = 20 * cos(this->ang * 0.0174533);
+			double incy = 20 * sin(this->ang * 0.0174533);
+			this->x = init_x + incx;
+			this->y =  init_y + incy;
+		}
+		void update(){
+			double incx = this->acel * cos(this->ang * 0.0174533);
+			double incy = this->acel * sin(this->ang * 0.0174533);
+			this->x += incx;
+			this->y += incy;
+			if (this->x <= 0){
+				this->x = SCREEN_WIDTH;
+			} else if (this->x >= SCREEN_WIDTH){
+				this->x = 0;
+			}
+			if (this->y <= 0){
+				this->y = SCREEN_HEIGHT;
+			} else if (this->y >= SCREEN_HEIGHT){
+				this->y = 0;
+			}
+			//SDL_Log("gun1.x: %0.10lf, gun1.y: %0.10lf", this->x, this->y);	
+		}
+	};// gun1(300,150, 45);
+
+	std::vector<Gun> bullets;
 
 	bool quit = false; // Variable para el ciclo del juego
 	SDL_Event e; // Variable para eventos
@@ -82,7 +116,8 @@ int main(int argc, char **argv)
 
 				}
 				if (state[SDL_SCANCODE_SPACE]) {
-				    //SDL_Log("<SPACE> is pressed.\n");
+					//SDL_Log("<SPACE> is pressed.\n");
+					bullets.push_back(Gun(player.x,player.y,player.angle));
 				}
 				if (player.angle <= 0)
 					{
@@ -131,14 +166,26 @@ int main(int argc, char **argv)
 			if (player.aceleracion > 0)
 			{
 				player.aceleracion -= player.aceleracion * 0.25;
-				SDL_Log("des aceleracion: %0.10lf", player.aceleracion);
+				//SDL_Log("des aceleracion: %0.10lf", player.aceleracion);
 			} else {
 				player.aceleracion = 0;
 			}
 		}
+		// Update Objects
 		player.update();
+		// asteroids.update();
+		for (int i = 0; i < bullets.size(); ++i)
+		{
+			if (bullets[i].t <= SDL_GetTicks() - 800)
+			{
+				bullets.erase(bullets.begin());
+			} else {
+				bullets[i].update();
+			}
+		}
 
 		// Mostrar en pantalla
+		// Player
 	    // Copiar la imagen en la surface de la ventana
 	    SDL_Point centerPlayer = {25,25};
 	    SDL_Rect srcRectPlayer = {168,156,209-168,193-156};
@@ -148,11 +195,29 @@ int main(int argc, char **argv)
 	    SDL_Rect dstRectPlayerC2 = {player.x-25 + SCREEN_WIDTH,player.y-25,50,50};
 	    SDL_Rect dstRectPlayerC3 = {player.x-25,player.y-25 - SCREEN_HEIGHT,50,50};
 	    SDL_Rect dstRectPlayerC4 = {player.x-25,player.y-25 + SCREEN_HEIGHT,50,50};
-		SDL_RenderCopyEx(render, texturePlayer, &srcRectPlayer, &dstRectPlayer, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
-		SDL_RenderCopyEx(render, texturePlayer, &srcRectPlayer, &dstRectPlayerC1, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
-		SDL_RenderCopyEx(render, texturePlayer, &srcRectPlayer, &dstRectPlayerC2, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
-		SDL_RenderCopyEx(render, texturePlayer, &srcRectPlayer, &dstRectPlayerC3, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
-		SDL_RenderCopyEx(render, texturePlayer, &srcRectPlayer, &dstRectPlayerC4, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(render, textureTileset, &srcRectPlayer, &dstRectPlayer, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(render, textureTileset, &srcRectPlayer, &dstRectPlayerC1, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(render, textureTileset, &srcRectPlayer, &dstRectPlayerC2, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(render, textureTileset, &srcRectPlayer, &dstRectPlayerC3, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
+		SDL_RenderCopyEx(render, textureTileset, &srcRectPlayer, &dstRectPlayerC4, player.angle + 90, &centerPlayer, SDL_FLIP_NONE);
+
+		// Bullets
+		SDL_Point centerGun1 = {4,9};
+		SDL_Rect srcRectGun1 = {140,255,148-140,274-255};
+		for (int i = 0; i < bullets.size(); ++i)
+		{
+			Gun* gun1 = &bullets[i];
+		    SDL_Rect dstRectGun1 = {gun1->x-4,gun1->y-9,8,19}; // Coordenadas y dimensiones en pantalla
+			SDL_Rect dstRectGunC1 = {gun1->x-4 - SCREEN_WIDTH,gun1->y-9,8,19};
+		    SDL_Rect dstRectGunC2 = {gun1->x-4 + SCREEN_WIDTH,gun1->y-9,8,19};
+		    SDL_Rect dstRectGunC3 = {gun1->x-4,gun1->y-9 - SCREEN_HEIGHT,8,19};
+		    SDL_Rect dstRectGunC4 = {gun1->x-4,gun1->y-9 + SCREEN_HEIGHT,8,19};
+			SDL_RenderCopyEx(render, textureTileset, &srcRectGun1, &dstRectGun1, gun1->ang + 90, &centerGun1, SDL_FLIP_NONE);
+			SDL_RenderCopyEx(render, textureTileset, &srcRectGun1, &dstRectGunC1, gun1->ang + 90, &centerGun1, SDL_FLIP_NONE);
+			SDL_RenderCopyEx(render, textureTileset, &srcRectGun1, &dstRectGunC2, gun1->ang + 90, &centerGun1, SDL_FLIP_NONE);
+			SDL_RenderCopyEx(render, textureTileset, &srcRectGun1, &dstRectGunC3, gun1->ang + 90, &centerGun1, SDL_FLIP_NONE);
+			SDL_RenderCopyEx(render, textureTileset, &srcRectGun1, &dstRectGunC4, gun1->ang + 90, &centerGun1, SDL_FLIP_NONE);
+		}
 
 		// Renderizar (redibujar la pantalla) y limpiar (el render no la pantalla)
 		SDL_RenderPresent(render);
@@ -165,8 +230,8 @@ int main(int argc, char **argv)
 	}
 	//SDL_Delay(1000);
 	// Limpar memoria dinamica y SDL
-	SDL_DestroyTexture(texturePlayer);
-	texturePlayer = NULL;
+	SDL_DestroyTexture(textureTileset);
+	textureTileset = NULL;
 	SDL_FreeSurface(surface);
 	surface = NULL;
 	SDL_DestroyRenderer(render);
